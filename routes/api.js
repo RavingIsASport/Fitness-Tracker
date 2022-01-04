bd9a6ee509daf2c6756b4912c4c5e2a01791ea8d
Gustavo Garcia <gustavogarcia42@yahoo.com>
2022-01-04 14:43:28 -0600

    Finished Api js and added index js

--- routes/api.js
+++ routes/api.js
@@ -25,10 +25,15 @@ router.put("/api/workout/:id", ({ body, params }, res) => {
     });
 });
 router.get("/api/workout", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then((dbTransaction) => {
-      res.json(dbTransaction);
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "duration" },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
     .catch((err) => {
       res.status(400).json(err);
