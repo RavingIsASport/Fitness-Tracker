33979f0747eee9946a9537cb730c53c15f348b88
Gustavo Garcia <gustavogarcia42@yahoo.com>
2022-01-04 14:14:04 -0600

    WOkred on api js

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,36 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("api");
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.put("/api/workout/:id", ({ body, params }, res) => {
+  // console.log(body, params);
+  Workout.findByIdAndUpdate(
+    { _id: params.id },
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.get("/api/workout", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((dbTransaction) => {
+      res.json(dbTransaction);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
